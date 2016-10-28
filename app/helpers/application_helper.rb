5b2150acc61f0a1fc357b30f949e9ed1cc3b373c
Ted Sappington <tedsappington@gmail.com>
2016-10-27 18:19:05 -0700

    9 Markdown: completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new()
+    Redcarpet::Markdown.new(renderer, extensions = {}).render(text).html_safe
+  end
+
 end
